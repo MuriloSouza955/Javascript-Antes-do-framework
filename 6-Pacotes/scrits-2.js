e82440dd9dca64df193857020659bf86cf181c0a
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-02 16:26:24 -0300

    Finalizando as aulas de bibliotecas (packages)

---
+++ 6-Pacotes/scrits-2.js
@@ -0,0 +1,14 @@
+/*
+Gerenciadores de pacote
+Nesta aula, vamos abordar o conceito de gerenciador de pacotes, destacando sua importância na instalação, atualização e gerenciamento de dependências de projetos. Veremos o NPM como um dos gerenciadores mais populares para JavaScript, utilizado tanto no ecossistema Node quanto em outros ambientes. Também aprenderemos a diferença entre dependências de produção e desenvolvimento, ressaltando a importância de manter as bibliotecas atualizadas e gerenciar as dependências do projeto. O próximo passo será explorar na prática o uso dos gerenciadores de pacotes.
+*/
+
+/*
+Conhecendo o node e o npm
+Vamos utilizar o NPM, gerenciador de pacotes do Node, para incluir pacotes na aplicação.
+*/
+
+/*
+Resolução de dependências
+Nesta aula, veremos sobre resolução de dependências, abordando o versionamento semântico, que facilita entender mudanças em versões de software. Cada número tem significados específicos. O NPM utiliza símbolos como ~ para permitir atualizações automáticas compatíveis, o ^ para instalar a versão mais recente compatível e o @ para instalar uma versão específica do pacote. Essas convenções garantem a compatibilidade e facilitam a gestão de bibliotecas em projetos.
+*/
