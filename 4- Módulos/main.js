7206362710709c9aa0df2ea7ce608a6e17ea4e5b
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-01 22:35:34 -0300

    Exportação padrão ou nomeada2

--- 4- Módulos/main.js
+++ 4- Módulos/main.js
@@ -9,8 +9,3 @@ import batata, {multiply} from './calc.js';
 
 console.log( "4 + 6 = ", batata(4, 6) );
 console.log( "4 * 6 = ", multiply(4, 6) );
-// pouco importa o nome da função default
-import batata, {multiply} from './calc.js';
-
-console.log( "4 + 6 = ", batata(4, 6) );
-console.log( "4 * 6 = ", multiply(4, 6) );
