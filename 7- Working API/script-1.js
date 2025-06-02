4d45dadcc9ef23a73d706413c280c104694bdb83
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-02 20:49:10 -0300

    Instalando e configurando o json-server

---
+++ 7- Working API/script-1.js
@@ -0,0 +1,13 @@
+/*
+
+Iniciando com requisições
+Nesta etapa, você aprenderá a fazer requisições HTTP para uma API e consumir os dados utilizando o fetch no JavaScript. Vai aprender a ler e enviar dados para a API, como cadastrar um novo produto, por exemplo.
+
+
+O que é uma API?
+Nessa aula aprenderemos o conceito de API e sua importância. Destacaremos exemplos de uso de APIs, como a de previsão do tempo e cotação de moedas, e a necessidade de seguir padrões de comunicação, como o JSON (JavaScript Object Notation). Conheceremos também os métodos HTTP utilizados em requisições, como GET, POST, PUT, DELETE e PATCH. APIs podem ser gratuitas ou pagas, permitindo integrações e uso de recursos externos em aplicações.
+
+Instalando o json-server
+Nesta aula, usaremos o json-server para simular uma API localmente, permitindo concentrar no consumo da API em JavaScript. Aprenderemos a o json-server, criar um arquivo server.json com dados simulados e executar o servidor localmente.
+*/
+
