5132533b2de683646565cb802f56e8cfcde9285e
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-02 10:49:36 -0300

    Conhecendo o setInterval

---
+++ 5-Funcoes assincronas/script-3.js
@@ -0,0 +1,5 @@
+/*
+Conhecendo funções assíncronas
+Nesta aula, você vai aprender sobre funções assíncronas e como elas retornam promises ao invés de valores imediatos. Funções assíncronas são úteis quando precisamos lidar com operações que podem demorar, como acessar um banco de dados. Elas retornam uma promessa de que vão processar a solicitação e retornar o resultado. O uso do await em funções assíncronas permite esperar pela resolução da promessa antes de continuar a execução do código. Esses conceitos serão exemplificados na prática durante a aula.
+*/
+
