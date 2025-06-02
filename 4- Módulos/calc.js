3e62267c28a3118f5228380fc74383b6f3ff3365
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-01 22:34:47 -0300

    Exportação padrão ou nomeada

--- 4- Módulos/calc.js
+++ 4- Módulos/calc.js
@@ -1,9 +1,9 @@
-function sum(a, b){
+// Funcão default é exportada por padrão
+export default function sum(a, b){
     return a + b;
 }
 
-function multiply(a, b){
+// named exports - cada método é importado pelo seu próprio nome de exportação
+export function multiply(a, b){
     return a * b;
 }
-
-export {sum, multiply};
--- 4- Módulos/main.js
+++ 4- Módulos/main.js
@@ -1,4 +1,16 @@
-import * as calc from './calc.js';
+/*
+Exportação padrão ou nomeada
+Nessa aula, veremos a diferença entre exportação padrão e nomeada em JavaScript. Veremos como exportar funções nomeadas e a função padrão, e como importá-las de forma separada. Destacaremos que a exportação default não requer chaves ao importar e que as funções nomeadas precisam ser importadas pelo nome exato.
 
-console.log( "4 + 6 = ", calc.sum(4, 6) );
-console.log( "4 * 6 = ", calc.multiply(4, 6) );
+*/
+
+// pouco importa o nome da função default
+import batata, {multiply} from './calc.js';
+
+console.log( "4 + 6 = ", batata(4, 6) );
+console.log( "4 * 6 = ", multiply(4, 6) );
+// pouco importa o nome da função default
+import batata, {multiply} from './calc.js';
+
+console.log( "4 + 6 = ", batata(4, 6) );
+console.log( "4 * 6 = ", multiply(4, 6) );
