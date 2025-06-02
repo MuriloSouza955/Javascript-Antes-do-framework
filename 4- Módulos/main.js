25740ce12cd4fd9909d02cd7e4e96d86f2e5291d
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-01 22:49:58 -0300

    Usando classes nos módulos

--- 4- Módulos/main.js
+++ 4- Módulos/main.js
@@ -1,15 +1,11 @@
 /*
-Renomeando as importações
-Nesta aula, aprenderemos a renomear a importação de funções em JavaScript. É importante renomear as importações para evitar conflitos de nomes com funções já existentes. Podemos renomear as importações para letras ou palavras diferentes, tornando o código mais claro e organizado. Renomear a importação é útil quando importamos módulos com nomes semelhantes aos que já estamos utilizando. Essa prática ajuda a evitar conflitos e a manter o código mais legível.
+Usando classes nos módulos
+Neste vídeo, vamos aprender sobre o uso de classes em módulos. Vamos criar uma classe, exportá-la e adicionar métodos dentro dela.
 */
 
-// pouco importa o nome da função default
-import {sum as soma, multiply as multi} from './calc.js';
+import { Calc } from "./calc.js";
 
-console.log( "4 + 6 = ", soma(4, 6) );
-console.log( "4 * 6 = ", multi(4, 6) );
+const calc = new Calc();
 
-// caso nao tivesse renomeado a funçao, poderia dar erro, pois ja existe uma funçao com o nome multiply
-function multiply(a, b) {
-    return a * b;
-}
+console.log("4 + 6 = ", calc.sum(4, 6));
+console.log("4 * 6 = ", calc.multiply(4, 6));
