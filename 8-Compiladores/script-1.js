8cbb43900ce2a77464ee5a8ee3da1b58b8c6d170
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-03 19:58:54 -0300

    Um pouco de motivação

---
+++ 8-Compiladores/script-1.js
@@ -0,0 +1,5 @@
+/*
+Um pouco de motivação
+Nessa aula, veremos a importância de configurar ferramentas do zero ao utilizar frameworks como React. Ao criar um projeto, o framework traz muitas configurações prontas para agilizar o desenvolvimento, mas entender o porquê por trás dessas configurações é fundamental. Veremos como instalar e configurar ferramentas como compiladores e bundlers do zero, para que você compreenda o funcionamento nos bastidores. Isso será útil para ter clareza ao utilizar o React no futuro.
+*/
+
