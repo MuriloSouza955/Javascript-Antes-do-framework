f47d9fffa074ec269efb007f41ad3d9744d0846b
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-03 21:36:38 -0300

    Aulas 1 e 2

--- 8-Compiladores/script-1.js
+++ 8-Compiladores/script-1.js
@@ -3,3 +3,8 @@ Um pouco de motivação
 Nessa aula, veremos a importância de configurar ferramentas do zero ao utilizar frameworks como React. Ao criar um projeto, o framework traz muitas configurações prontas para agilizar o desenvolvimento, mas entender o porquê por trás dessas configurações é fundamental. Veremos como instalar e configurar ferramentas como compiladores e bundlers do zero, para que você compreenda o funcionamento nos bastidores. Isso será útil para ter clareza ao utilizar o React no futuro.
 */
 
+/*
+Conhecendo os compiladores
+Nesta aula, veremos sobre compiladores em JavaScript, destacando a importância de converter código para versões compatíveis com navegadores mais antigos. Abordaremos o conceito de transpilação, que consiste em adaptar o código para versões anteriores da linguagem. Veremos as três etapas do processo: parser, transformer e generator. Aprenderemos a vantagem de automatizar esse processo com compiladores, permitindo o uso de recursos mais recentes da linguagem. Isso contribui para a evolução da linguagem e facilita a interoperabilidade entre diferentes ferramentas e bibliotecas.
+*/
+
