abc09b3649a0683d5ec8efa3e785521ffae222f6
MuriloSouza955 <souza.murilo@gmail.com>
2025-06-05 21:41:37 -0300

    Incluindo o CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
-const path = require('path');
-const HTMLwebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLwebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, '9-Bundlers', 'src', 'js', 'index.js'),
-    output: {
-        filename: 'main.js',
-        path: require('path').resolve(__dirname, 'dist')
-    },
-    mode: 'development',
-    plugins: [ new HTMLwebpackPlugin()]
+  entry: path.resolve(__dirname, "9-Bundlers", "src", "js", "index.js"),
+  output: {
+    filename: "main.js",
+    path: require("path").resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  plugins: [new HTMLwebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules/",
+      },
+    ],
+  },
 };
